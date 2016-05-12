8d60fba590ad0c0d18ca9075819327d9783280fd
JordanVazquezGarcia <jordan3f.dsaupoo@gmail.com>
2016-05-12 13:22:41 -0500

    3ersubida

--- js/acciones.js
+++ js/acciones.js
@@ -1,6 +1,9 @@
 // JavaScript Document
 $(document).ready(function(e) {
     document.addEventListener('deviceready', function() {
+		$('#beep').tap(function () {
+			navigator.notification.beep(1);
+		});//beep
 		$('#btnvibrar').on('tap', function () {
 			navigator.notification.vibrate(5555);
 		});
